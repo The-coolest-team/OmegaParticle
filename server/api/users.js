6c8fc7752f67bf747b40d52f00cdac2305077073
Timothy Parks Lynch <tsplynch@gmail.com>
2022-01-13 18:12:53 -0500

    Security on backend is started

--- server/api/users.js
+++ server/api/users.js
@@ -1,58 +1,61 @@
-const router = require('express').Router()
-const { models: { User } } = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
+const { requireToken, isAdmin } = require("./gateKeepingMiddleware.js");
+module.exports = router;
 
 // FIND ALL USERS
-router.get('/', async (req, res, next) => {
+router.get("/", requireToken, isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.send(users)
+      attributes: ["id", "username"],
+    });
+    res.send(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // FIND ONE USER
-router.get('/:userId', async (req, res, next) => {
+router.get("/:userId", async (req, res, next) => {
   try {
-    res.send(await User.findByPk(req.params.userId))
+    res.send(await User.findByPk(req.params.userId));
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // CREATE A USER
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
-    res.status(201).send(await User.create(req.body))
+    res.status(201).send(await User.create(req.body));
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // MODIFY A USER
-router.put('/:userId', async (req, res, next) => {
+router.put("/:userId", async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.userId)
-    await user.update(req.body)
-    res.send(user)
+    const user = await User.findByPk(req.params.userId);
+    await user.update(req.body);
+    res.send(user);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // DELETE A USER
-router.delete('/:userId', async (req, res, next) => {
+router.delete("/:userId", async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.userId)
-    await user.destroy()
-    res.send(user)
+    const user = await User.findByPk(req.params.userId);
+    await user.destroy();
+    res.send(user);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
