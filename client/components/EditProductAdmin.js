df23e029b94ab477ba5fd970c88cbb2e16125b32
Jinyoung Kim <jinyk226@gmail.com>
2022-01-14 13:39:47 -0800

    adminfrontend updated

---
+++ client/components/EditProductAdmin.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+export class EditProductAdmin extends React.Component {
+  constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     name: "",
+  //     description: "",
+  //     stock: 0,
+  //     price: 0
+  //   };
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  //   this.handleChange = this.handleChange.bind(this);
+  // }
+
+  // componentDidUpdate(prevProps) {
+  //   console.log(prevProps)
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+
+  // }
+
+  // handleChange(event) {
+  //   this.setState({
+  //     [event.target.name]: event.target.value,
+  //   });
+  // }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <h1>UPDATE THIS PRODUCT</h1>
+  //       <form onSubmit={this.handleSubmit}>
+  //         <label>Name:</label>
+  //         <input
+  //           name="name"
+  //           value={this.state.name}
+  //           onChange={this.handleChange}
+  //         />
+  //         <br />
+  //         <label>Description:</label>
+  //         <input
+  //           name="description"
+  //           value={this.state.description}
+  //           onChange={this.handleChange}
+  //         />
+  //         <br />
+  //         <label>Price:</label>
+  //         <input
+  //           name="price"
+  //           value={this.state.price}
+  //           onChange={this.handleChange}
+  //         />
+  //         <br />
+  //         <label>Stock:</label>
+  //         <input
+  //           name="stock"
+  //           value={this.state.stock}
+  //           onChange={this.handleChange}
+  //         />
+  //         <br />
+  //         <button type="submit">Update</button>
+  //       </form>
+  //     </div>
+  //   );
+  }
+}
+
+export default EditProductAdmin;
